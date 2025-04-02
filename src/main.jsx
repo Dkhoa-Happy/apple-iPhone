9c651fb7f0537392c89582410a455845d10778e8
Dang Khoa <ojtgrowth@sendo.vn>
2025-04-02 11:39:15 +0700

    update Sentry configuration and dependencies; add additional Sentry project

--- src/main.jsx
+++ src/main.jsx
@@ -3,27 +3,27 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-//...
 import * as Sentry from "@sentry/react";
 
 Sentry.init({
-  dsn: "https://d674932a77e6d9b9ced1190d70fd4691@o4506876178464768.ingest.us.sentry.io/4506876181151744",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.metrics.metricsAggregatorIntegration(),
-    Sentry.reactRouterV6BrowserTracingIntegration({
-      useEffect: React.useEffect,
-    }),
-    Sentry.replayIntegration({
-      maskAllText: false,
-      blockAllMedia: false,
-    }),
-  ],
-  tracesSampleRate: 1.0, 
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0, 
+  dsn: "https://3d3e323c96825405d77b5a6330445cc3@o4508410080919552.ingest.us.sentry.io/4509081725239296",integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.metrics.metricsAggregatorIntegration(),
+        Sentry.reactRouterV6BrowserTracingIntegration({
+            useEffect: React.useEffect,
+        }),
+        Sentry.replayIntegration({
+            maskAllText: false,
+            blockAllMedia: false,
+        }),
+    ],
+    tracesSampleRate: 1.0,
+    tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
 });
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
